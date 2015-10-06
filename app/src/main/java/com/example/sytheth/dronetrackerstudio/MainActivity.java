b31856bb01c80fdc1a6ec87d333a3b6f03103696
Sytheth <sytheth@gmail.com>
2015-10-05 21:18:13 -0400

    Added Basic GUI Layout XML and appropriate button functions

--- app/src/main/java/com/example/sytheth/dronetrackerstudio/MainActivity.java
+++ app/src/main/java/com/example/sytheth/dronetrackerstudio/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -34,4 +35,12 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void sendEmail(View view){
+
+    }
+    public void takePhoto(View view){
+
+    }
+
+
 }
