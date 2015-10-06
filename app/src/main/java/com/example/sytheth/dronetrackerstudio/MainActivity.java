f221cb02546a2d88959d23103a7593cc7f7a734f
Sytheth <sytheth@gmail.com>
2015-10-05 22:40:35 -0400

    Added Camera to GUI and basic preview window with back-facing camera

--- app/src/main/java/com/example/sytheth/dronetrackerstudio/MainActivity.java
+++ app/src/main/java/com/example/sytheth/dronetrackerstudio/MainActivity.java
@@ -1,43 +1,187 @@
 package com.example.sytheth.dronetrackerstudio;
 //testing in class for the hundreth time
+import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.Surface;
+import android.view.TextureView;
+import android.graphics.SurfaceTexture;
+import android.util.Size;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+
+import android.hardware.camera2.params.StreamConfigurationMap;
+
+import android.content.pm.PackageManager;
+import java.util.Collections;
+
+import android.os.HandlerThread;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
+    private TextureView mTextureView = null;
+    private Size mPreviewSize = null;
+    private CameraDevice mCameraDevice = null;
+    private CaptureRequest.Builder mPreviewBuilder = null;
+    private CameraCaptureSession mPreviewSession = null;
+
+
+    // Create the Surface Texture Listener
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width,
+                                                int height) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            // TODO Auto-generated method stub
+            Toast.makeText(MainActivity.this, "Destroyed!", Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width,
+                                              int height) {
+            // TODO Auto-generated method stub
+            CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+            try{
+                String cameraId = manager.getCameraIdList()[0];
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                try {
+                    mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+                }
+                catch(NullPointerException e){
+                    e.printStackTrace();
+                }
+                //TODO finish what happens if permission is denied for camera
+                if (checkCallingOrSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                    manager.openCamera(cameraId, mStateCallback, null);
+
+                else{
+                    Toast.makeText(MainActivity.this, "Error 04!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch(CameraAccessException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(CameraDevice camera) {
+            // TODO Auto-generated method stub
+            mCameraDevice = camera;
+
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            if (texture == null) {
+                return;
+            }
+
+            texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface surface = new Surface(texture);
+
+            try {
+                mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+            mPreviewBuilder.addTarget(surface);
+
+            try {
+                mCameraDevice.createCaptureSession(Collections.singletonList(surface), mPreviewStateCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            // TODO Auto-generated method stub
+            Toast.makeText(MainActivity.this, "Error 01!", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            // TODO Auto-generated method stub
+        }
+    };
+
+    private CameraCaptureSession.StateCallback mPreviewStateCallback = new CameraCaptureSession.StateCallback() {
+
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            // TODO Auto-generated method stub
+            mPreviewSession = session;
+            mPreviewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+            HandlerThread backgroundThread = new HandlerThread("CameraPreview");
+            backgroundThread.start();
+            Handler backgroundHandler = new Handler(backgroundThread.getLooper());
+
+            try {
+                mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(), null, backgroundHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+
+            }
+
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+            // TODO Auto-generated method stub
+            Toast.makeText(MainActivity.this, "Error02!",Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        mTextureView = (TextureView) findViewById(R.id.textureView1);
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onPause(){
+        super.onPause();
+        if (mCameraDevice != null)
+        {
+            mCameraDevice.close();
+            mCameraDevice = null;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
     public void sendEmail(View view){
 
     }
+
     public void takePhoto(View view){
 
     }
