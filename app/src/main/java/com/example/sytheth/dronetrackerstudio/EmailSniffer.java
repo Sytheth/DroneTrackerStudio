1566f586a8afd77d4c99098d6e23eb0c04e5beab
Stephen <cronins1@my.erau.edu>
2015-10-25 13:20:06 -0400

    Reworked so it will read all unread messages, verify they are relevant, and mark them as read. Relevant data is now parsed and logged to a file

--- app/src/main/java/com/example/sytheth/dronetrackerstudio/EmailSniffer.java
+++ app/src/main/java/com/example/sytheth/dronetrackerstudio/EmailSniffer.java
@@ -1,18 +1,20 @@
-package com.example.sytheth.dronetrackerstudio;
-
 import java.util.*;
 import javax.activation.DataHandler;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.mail.*;
+import javax.mail.Flags.Flag;
+import javax.mail.search.FlagTerm;
+
 import org.apache.commons.lang3.StringUtils;
 import java.io.InputStream;
 import java.io.FileOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 
 
 /**
- * 
+ *
  * @author Stephen Cronin
  * Things to implement next:
  * 		-Writing to file
@@ -31,63 +33,83 @@ public class EmailSniffer {
 			Store store = session.getStore();
 			store.connect("imap.gmail.com", "croninstephen347@gmail.com", "Stephen55!");
 			Folder inbox = store.getFolder("INBOX");
-			inbox.open(Folder.READ_ONLY);
-			Message msg = inbox.getMessage(inbox.getMessageCount());
-			Address[] in = msg.getFrom();
-			for (Address address : in) {
-				System.out.println("FROM:" + address.toString());
-			}
+			inbox.open(Folder.READ_WRITE);
 
-			Multipart mp = (Multipart) msg.getContent();
-			BodyPart bp = mp.getBodyPart(0);
-			System.out.println("SENT DATE:" + msg.getSentDate());
-			System.out.println("SUBJECT:" + msg.getSubject());
-			//System.out.println("CONTENT:" + bp.getContent());
+			Message messages[] = inbox.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));//Looks for all unread emails
+			for (int z = 0; z < messages.length; z++){ //Loop through all emails
+				Message msg = messages[z];
+				Address[] in = msg.getFrom();
 
-			System.out.println("-------------------------");
-
-
-			List<File> attachments = new ArrayList<File>();
-			//for (Message message : msg) {
-			Multipart multipart = (Multipart) msg.getContent();
-			//Multipart multipart = (Multipart) message.getContent();
-			// System.out.println(multipart.getCount());
-
-			for (int i = 0; i < multipart.getCount(); i++) {
-				BodyPart bodyPart = multipart.getBodyPart(i);
-				if(!Part.ATTACHMENT.equalsIgnoreCase(bodyPart.getDisposition()) &&
-						!StringUtils.isNotBlank(bodyPart.getFileName())) {
-					continue; // dealing with attachments only
-				} 
-				InputStream is = bodyPart.getInputStream();
-				File f = new File("E:/Workspace/SE 300/" + bodyPart.getFileName());
-
-				try{
-					javaxt.io.Image image = new javaxt.io.Image(f);
-					double[] gps = image.getGPSCoordinate();
-					System.out.println("N: " + gps[1]); //N
-					System.out.println("W: " + gps[0]); //W
-				} catch(Exception e){//No coodinate data
-					System.out.println(e);
+				//Check to see the message came from a relevant address
+				Boolean sender = false;
+				for (Address address : in) {
+					if(address.toString().contains("gdcerau@gmail.com") || address.toString().contains("devin@isovirtual.com")){
+						System.out.println("FROM:" + address.toString());
+						sender = true;
+					}
 				}
 
-				FileOutputStream fos = new FileOutputStream(f);
-				byte[] buf = new byte[4096];
-				int bytesRead;
-				while((bytesRead = is.read(buf))!=-1) {
-					fos.write(buf, 0, bytesRead);
-				}
-				fos.close();
-				attachments.add(f);
-			}
-			//}
-		
+				if(sender){//Sent from a source we care about
+					//Prepare database write
+					FileWriter fileWritter = new FileWriter("Database.txt",true);
+					BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+					String writeString = "";
+
+					//Pull data from message
+					Multipart mp = (Multipart) msg.getContent();
+					BodyPart bp = mp.getBodyPart(0);
+					//System.out.println("SENT DATE:" + msg.getSentDate());
+					//System.out.println("SUBJECT:" + msg.getSubject());
+
+					writeString = writeString + msg.getSubject() + "\t";
+
+					//System.out.println("CONTENT:" + bp.getContent());
+					//System.out.println("-------------------------");
+
+
+					//Get the picture from the email
+					List<File> attachments = new ArrayList<File>();
+					Multipart multipart = (Multipart) msg.getContent();
+					for (int i = 0; i < multipart.getCount(); i++) {
+						BodyPart bodyPart = multipart.getBodyPart(i);
+						if(!Part.ATTACHMENT.equalsIgnoreCase(bodyPart.getDisposition()) &&
+								!StringUtils.isNotBlank(bodyPart.getFileName())) {
+							continue; // dealing with attachments only
+						}
+						InputStream is = bodyPart.getInputStream();
+						File f = new File("E:/Documents/Workspace/SE 300 Database/" + bodyPart.getFileName());
+						try{
+							//Pull GPS data from image
+							javaxt.io.Image image = new javaxt.io.Image(f);
+							double[] gps = image.getGPSCoordinate();
+							writeString = writeString + "N: " + gps[1] + "\t" + "W: " + gps[0] + "\t";
+						} catch(Exception e){//No coordinate data
+							writeString = writeString + "Coordinate data not available.\t";
+						}
+						//Write image file and save it
+						FileOutputStream fos = new FileOutputStream(f);
+						byte[] buf = new byte[4096];
+						int bytesRead;
+						while((bytesRead = is.read(buf))!=-1) {
+							fos.write(buf, 0, bytesRead);
+						}
+						fos.close();
+						attachments.add(f);
+						writeString = writeString + f.getPath();//Last entry in database
+					}
+
+					bufferWritter.write(writeString);//Write entry to database
+					bufferWritter.newLine();//Next entry goes on next line
+					bufferWritter.close();
+					System.out.println("-------------------" + "\n");
+
+				}//End if
+
+				msg.setFlag(Flags.Flag.SEEN, true); //Marks the email as read
+			}//End for
+
 		} catch (Exception mex) {
 			mex.printStackTrace();
 		}
-	}
-
-	public static void fileLogger(){
-
 	}
 }
