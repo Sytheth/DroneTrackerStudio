39621ec696e756acd7c7d26a46aba4bfe758304e
awalts <Kathy209>
2015-10-26 13:07:12 -0400

    Saving photo code implemeted

--- app/src/main/java/com/example/sytheth/dronetrackerstudio/MainActivity.java
+++ app/src/main/java/com/example/sytheth/dronetrackerstudio/MainActivity.java
@@ -4,14 +4,22 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
+import android.media.ExifInterface;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.Surface;
 import android.view.TextureView;
@@ -24,7 +32,15 @@ import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import android.os.HandlerThread;
 import android.os.Handler;
@@ -43,6 +59,9 @@ public class MainActivity extends Activity implements LocationListener {
     private CameraCaptureSession mPreviewSession = null;
     private Size mPreviewSize;
     public Location location;
+
+    // Storage location of image file
+    final File file = new File(Environment.getExternalStorageDirectory()+"/DCIM/Camera", "IMG_Drone.jpg");
 
     {
         mPreviewSize = null;
@@ -247,9 +266,170 @@ public class MainActivity extends Activity implements LocationListener {
     public void takePhoto(View view){
         // Get the GPS Location
         LocationManager locationMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
         locationMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        // Check to see if camera is open
+        if(null == mCameraDevice) {
+            return;
+        }
+        // Capture the picture
+        try {
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(mCameraDevice.getId());
+            Size[] jpegSizes = null;
+
+            // Set up camera type
+            if (characteristics != null) {
+                jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
+            }
+
+            // This does NOT set the image size, but it is a guess if all else fails
+            int width = 480;
+            int height = 480;
+
+            // Choose the best quality image available from the camera
+            if (jpegSizes != null && 0 < jpegSizes.length) {
+                width = jpegSizes[0].getWidth();
+                height = jpegSizes[0].getHeight();
+            }
+
+            // Read the image from the camera
+            ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+            List<Surface> outputSurfaces = new ArrayList<Surface>(2);
+            outputSurfaces.add(reader.getSurface());
+            outputSurfaces.add(new Surface(mTextureView.getSurfaceTexture()));
+            final CaptureRequest.Builder captureBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            captureBuilder.addTarget(reader.getSurface());
+            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            // Set up a image available listener
+            ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image image = null;
+                    // Capture the image
+                    try {
+                        image = reader.acquireLatestImage();
+                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buffer.capacity()];
+                        buffer.get(bytes);
+                        // Save the image using the save method
+                        save(bytes);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (image != null) {
+                            image.close();
+                            // Wait 20 seconds to time out if GPS location could not be found
+                            int count=0;
+                            // GEO-tag the image if location was found
+                            if (location != null){
+                                loc2Exif(file.getPath(), location);
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this, "Failed to find location!", Toast.LENGTH_SHORT).show();
+                            }
+                            // Allow the picture to be sent
+                        }
+                    }
+                }
+
+                // Save the image
+                private void save(byte[] bytes) throws IOException {
+                    OutputStream output = null;
+                    try {
+                        output = new FileOutputStream(file);
+                        // Crop the image
+                        Bitmap bmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+                        Bitmap cropped = Bitmap.createBitmap(bmap,0,0,bmap.getWidth(),bmap.getWidth());
+                        cropped = Bitmap.createScaledBitmap(cropped, 480, 480, true);
+                        cropped.compress(Bitmap.CompressFormat.JPEG, 85, output);
+
+                    } finally {
+                        if (null != output) {
+                            output.close();
+                        }
+                    }
+                }
+
+            };
+
+            // Run the camera capture on a separate thread
+            HandlerThread thread = new HandlerThread("CameraPicture");
+            thread.start();
+            final Handler backgroudHandler = new Handler(thread.getLooper());
+            reader.setOnImageAvailableListener(readerListener, backgroudHandler);
+
+            // Create a capture listener for when the image has been saved
+            final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
+
+                @Override
+                public void onCaptureCompleted(CameraCaptureSession session,
+                                               CaptureRequest request, TotalCaptureResult result) {
+                    super.onCaptureCompleted(session, request, result);
+                    Toast.makeText(MainActivity.this, "Saved:"+file, Toast.LENGTH_SHORT).show();
+                }
+
+            };
+
+            // start the capture session
+            mCameraDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
+
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+
+                    try {
+                        session.capture(captureBuilder.build(), captureListener, backgroudHandler);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            }, backgroudHandler);
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+    // GEO-Tag the image
+    //http://stackoverflow.com/questions/10531544/write-geotag-jpegs-exif-data-in-android
+    public void loc2Exif(String flNm, Location loc) {
+
+        try {
+            ExifInterface ef = new ExifInterface(flNm);
+            ef.setAttribute(ExifInterface.TAG_GPS_LATITUDE, dec2DMS(loc.getLatitude()));
+            ef.setAttribute(ExifInterface.TAG_GPS_LONGITUDE,dec2DMS(loc.getLongitude()));
+            if (loc.getLatitude() > 0)
+                ef.setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, "N");
+            else
+                ef.setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, "S");
+            if (loc.getLongitude()>0)
+                ef.setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, "E");
+            else
+                ef.setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, "W");
+            ef.saveAttributes();
+            Toast.makeText(MainActivity.this, "Location Saved!", Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {}
+    }
+
+    // Reformat the GPS Data
+    String dec2DMS(double coord) {
+        coord = coord > 0 ? coord : -coord;  // -105.9876543 -> 105.9876543
+        String sOut = Integer.toString((int)coord) + "/1,";   // 105/1,
+        coord = (coord % 1) * 60;         // .987654321 * 60 = 59.259258
+        sOut = sOut + Integer.toString((int)coord) + "/1,";   // 105/1,59/1,
+        coord = (coord % 1) * 60000;             // .259258 * 60000 = 15555
+        sOut = sOut + Integer.toString((int)coord) + "/1000";   // 105/1,59/1,15555/1000
+        return sOut;
     }
 
 }
